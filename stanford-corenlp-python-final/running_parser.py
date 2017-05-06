6730fc79657f83375bb461d30b148ead2cf93a53
senticteam <iti@alumni.nus.edu.sg>
2017-05-07 01:50:47 +0800

    -

---
+++ stanford-corenlp-python-final/running_parser.py
@@ -0,0 +1,4 @@
+import concept_parser_line2
+
+a = concept_parser_v2()
+print a.parser("finally, for the beginner there are not enough conceptual clues on what is actually going on and it is hard to form any mental model of the underlying processes")
